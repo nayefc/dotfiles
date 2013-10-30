8551047a0bf620b1efaae287ba3254956fb6b35d
Nayef Copty <nrcopty@gmail.com>
2013-10-30 12:12:46 -0400

    Add MELPA to emacs and fiplr lib

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,15 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+ ;; Fiplr
+(global-set-key (kbd "C-x f") 'fiplr-find-file)
+(setq fiplr-ignored-globs '((directories (".git" ".svn"))
+			    (files ("*.jpg" "*.png" "*.zip" "*~"))))
+
 ;; Add Load Path Directory
 (add-to-list 'load-path "~/.emacs.d/")
 
