c7461547abec461731ab893e9ce11dfd14304d86
Nayef Copty <nrcopty@gmail.com>
2014-01-06 10:51:52 -0500

    Change cua-mode to cua-selection-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,7 @@
 ;; Add Load Path Directory
 (add-to-list 'load-path "~/.emacs.d/")
 
-(cua-mode t)
+(cua-selection-mode t)
 (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
 (transient-mark-mode 1) ;; No region when it is not highlighted
 (setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
