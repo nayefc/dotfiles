6fb839108be96643b79e050e6064315032fec604
Nayef Copty <nrcopty@gmail.com>
2014-10-21 12:23:58 -0400

    Remove Emacs iswitch. Install Solarized theme.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -7,9 +7,33 @@
 
 ; check if we're on OSX
 (when (featurep 'ns-win)
-    (custom-set-faces
-        '(default ((t (:height 140 :width normal :family "Inconsolata")))))
-      )
+
+  ;; Bring OS X Emacs in line with shell setup
+  (exec-path-from-shell-initialize)
+
+  ;; Load solarized theme
+  (load-theme 'solarized-dark t)
+
+  ;; make the fringe stand out from the background
+  (setq solarized-distinct-fringe-background t)
+
+  ;; make the modeline high contrast
+  (setq solarized-high-contrast-mode-line t)
+
+  ;; Use less bolding
+  (setq solarized-use-less-bold t)
+
+  ;; Use more italics
+  (setq solarized-use-more-italic t)
+
+  ;; Use less colors for indicators such as git:gutter, flycheck and similar.
+  (setq solarized-emphasize-indicators nil)
+
+  (setq x-underline-at-descent-line t)
+
+  (custom-set-faces
+   '(default ((t (:height 160 :width normal :family "Inconsolata")))))
+  )
 
 ;; Disable source control hooks (i.e: vc-git)
 (setq vc-handled-backends nil)
@@ -87,10 +111,6 @@
 
 ;; Go to line
 (global-set-key "\M-1" `goto-line)
-
-;; Use iswitch
-(iswitchb-mode 1)
-(setq iswitchb-buffer-ignore '("^ " "*"))
 
 ;; Display line numbers at the bottom
 (setq line-number-mode t)
@@ -225,3 +245,16 @@
 (add-hook 'js-mode-hook '(lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
