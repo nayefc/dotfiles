0e0331eb16471a0348b4dda98eb43b671e37eae3
Nayef Copty <nrcopty@gmail.com>
2015-04-29 11:28:47 -0400

    Add highlight-symbol mode to Emacs.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -269,5 +269,18 @@
     (setq projectile-completion-system 'helm)
     (setq projectile-switch-project-action 'projectile-dired)))
 
+(use-package highlight-symbol
+  :ensure t
+  :defer t
+  :bind (("C-c w" . highlight-symbol)
+	 ("C-c a" . highlight-symbol-next)
+	 ("C-c s" . highlight-symbol-prev)
+	 ("C-c q" . highlight-symbol-remove-all))
+  :config
+  (progn
+    (setq highlight-symbol-colors
+	  '("DeepPink" "cyan" "MediumPurple1" "SpringGreen1"
+	    "DarkOrange" "HotPink1" "RoyalBlue1" "OliveDrab"))))
+
 (provide 'init)
 ;;; init.el ends here
