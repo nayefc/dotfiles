b8382b5ec419496a78769b4c9c0d3fbf747d4d02
Nayef Copty <nrcopty@gmail.com>
2015-08-04 10:47:52 -0400

    OS X brew fixes.

--- bins/bootstrap.sh
+++ bins/bootstrap.sh
@@ -9,16 +9,8 @@ fi
 
 if [[ $platform == 'osx' ]]; then
     brew tap Homebrew/brewdler
-    brew install caskroom/cask/brew-cask
     brew brewdle
-    # brew install hr
-    # brew install pyenv
-    # brew install pyenv-virtualenv
-    # brew install jawshooah/pyenv/pyenv-default-packages
     echo -e "pylint\npylint_django\nipdb\njedi\n#jediepcserver" > ~/.pyenv/default-packages
-    # brew install youtube-dl
-    # brew install emacs
-    # brew install tmux
 # elif [[ $platform == 'linux' ]]; then
     # sudo cp bins/hr/hr /usr/local/bin/
     # sudo chmod +x /usr/local/bin/hr
