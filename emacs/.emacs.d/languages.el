13d95e104f673ae2392e9bca6981422d21fcbf10
Nayef Copty <nrcopty@gmail.com>
2015-09-01 12:00:17 -0400

    Change Emacs python keybindings.

--- emacs/.emacs.d/languages.el
+++ emacs/.emacs.d/languages.el
@@ -43,14 +43,14 @@
   (unless (search-forward (concat "class " classname) nil t nil)
     (search-backward (concat "class " classname "("))))
 ;;(bind-key "C-x a s" 'goto-python-class)
-(define-key python-mode-map (kbd "C-x a s") 'goto-python-class)
+(define-key python-mode-map (kbd "C-x a c") 'goto-python-class)
 
 (defun goto-python-function (funcname)
   "Search for a python function FUNCNAME in the current file."
   (interactive "sFunction name: ")
   (unless (search-forward (concat "def " funcname) nil t nil)
     (search-backward (concat "def " funcname "("))))
-(define-key python-mode-map (kbd "C-x a a") 'goto-python-function)
+(define-key python-mode-map (kbd "C-x a f") 'goto-python-function)
 
 ;; Indent JavaScript to 2
 (add-hook 'js-mode-hook
