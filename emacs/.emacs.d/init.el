7a62bc061054e8f685c6ae99f5580d99044ffa2f
Nayef Copty <nrcopty@gmail.com>
2015-09-18 18:15:48 -0400

    Change font to Inconsolata-dz

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,13 +8,21 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+    ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e"
+     "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879"
+     "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
+     default))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 150 :width normal :family "Monaco")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil
+			 :box nil :strike-through nil :overline nil
+			 :underline nil :slant normal
+			 :weight normal :height 140 :width normal
+			 :foundry "nil" :family "Inconsolata-dz")))))
 
 ;;; Code:
 
--- emacs/.emacs.d/osx.el
+++ emacs/.emacs.d/osx.el
@@ -42,24 +42,7 @@
     (setq x-underline-at-descent-line t)
 
     ;; Highlight current line
-    (global-hl-line-mode +1)
-
-    (custom-set-variables
-     ;; custom-set-variables was added by Custom.
-     ;; If you edit it by hand, you could mess it up, so be careful.
-     ;; Your init file should contain only one such instance.
-     ;; If there is more than one, they won't work right.
-     '(custom-safe-themes (quote ("e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e"
-				  "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879"
-				  "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
-				  default))))
-    (custom-set-faces
-     ;; custom-set-faces was added by Custom.
-     ;; If you edit it by hand, you could mess it up, so be careful.
-     ;; Your init file should contain only one such instance.
-     ;; If there is more than one, they won't work right.
-					; '(default ((t (:height 150 :width normal :family "Terminus"))))
-     '(default ((t (:height 150 :width normal :family "Monaco")))))))
+    (global-hl-line-mode +1)))
 
 ;; (if (display-graphic-p)
 ;;     (setq initial-buffer-choice "~/Documents/Percolate/devolate/hotlanta"))
