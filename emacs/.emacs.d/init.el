a30e56e572d525b4e30b36c31a7e85d0d5c914a1
Nayef Copty <nrcopty@gmail.com>
2015-11-30 11:31:37 -0500

    Update Cask link.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -24,7 +24,7 @@
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
-(require 'cask "/Users/nayefcopty/dotfiles/emacs/.emacs.d/.cask/24.5.1/elpa/cask-20151009.202/cask.el")
+(require 'cask "/Users/nayefcopty/dotfiles/emacs/.emacs.d/.cask/24.5.1/elpa/cask-20151123.528/cask.el")
 (cask-initialize)
 (require 'pallet)
 (pallet-mode t)
