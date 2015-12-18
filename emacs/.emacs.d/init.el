275d8058a9c0fcdb2b8456b6ff3efac4c0ce7873
Nayef Copty <nrcopty@gmail.com>
2015-12-17 20:27:07 -0500

    Minor change.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-'(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "*-Inconsolata-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "*-Inconsolata-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")))))
 
 ;; Code:
 
