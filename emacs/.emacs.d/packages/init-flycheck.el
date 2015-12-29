239d4762c4ecd39e10883bf5fc2399bdfd8aadec
Nayef Copty <nrcopty@gmail.com>
2015-12-29 15:23:22 +0200

    Fix Flycheck.

--- emacs/.emacs.d/packages/init-flycheck.el
+++ emacs/.emacs.d/packages/init-flycheck.el
@@ -1,6 +1,5 @@
 (use-package flycheck
   :ensure t
-  :commands global-flycheck-mode
   :init
   (setq flycheck-highlighting-mode 'lines)
   (setq flycheck-check-syntax-automatically '(mode-enabled new-line idle-change))
@@ -8,7 +7,7 @@
   :config
   (set-face-attribute 'flycheck-error nil :foreground "yellow" :background "red")
   (set-face-attribute 'flycheck-warning nil :foreground "red" :background "yellow")
-  (set-face-attribute 'flycheck-info nil :foreground "red" :background "yellow"))
-  ;;;;(global-flycheck-mode)
+  (set-face-attribute 'flycheck-info nil :foreground "red" :background "yellow")
+  (global-flycheck-mode))
 
 (provide 'init-flycheck)
