3bb902c2f00e89f954aa92253abf86431682ccd4
Nayef Copty <nrcopty@gmail.com>
2016-03-16 13:44:25 -0400

    Add more er/expand-region keybindings.

--- emacs/.emacs.d/packages/init-expand-region.el
+++ emacs/.emacs.d/packages/init-expand-region.el
@@ -1,5 +1,11 @@
 (use-package expand-region
   :ensure t
-  :bind ("C-c m" . er/expand-region))
+  :bind (("C-c m" . er/expand-region)
+	 ("C-x m" . er/mark-outer-python-block)
+	 ("C-x /" . er/mark-inside-python-string)
+	 ("C-x \\" . er/mark-outside-python-string)
+	 ("C-x l" . er/mark-python-statement)
+	 ("C-x h" . er/mark-inside-pairs)
+	 ("C-x j" . er/mark-outside-pairs)))
 
 (provide 'init-expand-region)
