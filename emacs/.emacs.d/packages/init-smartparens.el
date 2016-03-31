859c78faa6f331bb7091d67cf94f14d8fcb0757b
Nayef Copty <nrcopty@gmail.com>
2016-03-31 14:07:47 -0400

    Stop deactivating active sexp on backspace.

--- emacs/.emacs.d/packages/init-smartparens.el
+++ emacs/.emacs.d/packages/init-smartparens.el
@@ -4,7 +4,8 @@
   ;; :bind (("C-c <right>" . sp-forward-slurp-sexp)
   ;; 	 ("C-c <left>" . sp-forward-barf-sexp))
   :config
-  (require 'smartparens-config))
+  (require 'smartparens-config)
+  (setq sp-cancel-autoskip-on-backward-movement nil))
 
 (add-hook 'python-mode-hook 'turn-on-smartparens-strict-mode)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-smartparens-strict-mode)
