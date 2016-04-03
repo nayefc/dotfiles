3822ec94a6f76f56f7d9c1e0d26466b0e0822496
Nayef Copty <nrcopty@gmail.com>
2016-04-03 11:35:44 -0400

    Emacs fix.

--- emacs/.emacs.d/packages/init-smartparens.el
+++ emacs/.emacs.d/packages/init-smartparens.el
@@ -1,15 +1,17 @@
 (use-package smartparens-config
   :ensure smartparens
   :diminish smartparens-mode
+  :bind (("M-]" . sp-unwrap-sexp))
   ;; :bind (("C-c <right>" . sp-forward-slurp-sexp)
   ;; 	 ("C-c <left>" . sp-forward-barf-sexp))
   :config
-  (require 'smartparens-config)
-  (setq sp-cancel-autoskip-on-backward-movement nil))
+  (require 'smartparens-config))
 
 (add-hook 'python-mode-hook 'turn-on-smartparens-strict-mode)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-smartparens-strict-mode)
 (add-hook 'lisp-mode-hook 'turn-on-smartparens-strict-mode)
+
+(setq sp-cancel-autoskip-on-backward-movement nil)
 
 (--each '("python-mode" "python")
   (eval-after-load it '(require 'smartparens-python)))
