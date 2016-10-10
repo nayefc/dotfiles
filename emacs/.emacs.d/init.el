ecb6db1f128c4d2ba563ae21a9e2f1ac4ea27b3f
Nayef Copty <nayef@hudson-trading.com>
2016-10-10 15:24:00 -0400

    Add Emacs package.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -92,7 +92,7 @@
  '(exec-path-from-shell-check-startup-files nil)
  '(package-selected-packages
    (quote
-    (buffer-move pylint helm-projectile pyenv-mode highlight-indentation company-jedi jedi expand-region company projectile helm-git-grep helm exec-path-from-shell fill-column-indicator git-gutter+ flycheck highlight-symbol multiple-cursors ace-window avy magit yaml-mode with-editor use-package sr-speedbar solarized-theme smartparens pallet magit-popup))))
+    (virtualenvwrapper buffer-move pylint helm-projectile pyenv-mode highlight-indentation company-jedi jedi expand-region company projectile helm-git-grep helm exec-path-from-shell fill-column-indicator git-gutter+ flycheck highlight-symbol multiple-cursors ace-window avy magit yaml-mode with-editor use-package sr-speedbar solarized-theme smartparens pallet magit-popup))))
 
 ;; Font height 135 or 130
 (custom-set-faces
