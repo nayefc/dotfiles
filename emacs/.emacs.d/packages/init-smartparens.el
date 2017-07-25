a41ae108d100c231d09ab471258009c523b74c7c
Nayef Copty <nrcopty@gmail.com>
2017-07-25 11:50:21 -0400

    Change smartparens to use hydra.

--- emacs/.emacs.d/packages/init-smartparens.el
+++ emacs/.emacs.d/packages/init-smartparens.el
@@ -2,20 +2,88 @@
   :ensure t
   :pin melpa-stable
   :diminish smartparens-mode
-  :bind (("M-]" . sp-unwrap-sexp)
-	 ("C-M-f" . sp-forward-sexp)
-	 ("C-M-b" . sp-backward-sexp)
-	 ("C-M-<right>" . sp-forward-slurp-sexp)
-	 ("C-M-<left>" . sp-backward-slurp-sexp))
-  	 ;; ("C-<right>" . sp-forward-barf-sexp)
-	 ;; ("C-<left>" . sp-backward-barf-sexp)
-  :config
-  (require 'smartparens-config))
+  :bind (("C-c h" . hydra-smartparens/body)
+         :map smartparens-strict-mode-map
+	 ("M-]" . sp-unwrap-sexp)
+         ;; A fill paragraph in strict mode
+	 ("M-q" . sp-indent-defun))
+  :init
+  ;; Hydra for Smartparens
+  (defhydra hydra-smartparens (:hint nil)
+    "
+Sexps (quit with _q_)
+^Nav^            ^Barf/Slurp^                 ^Depth^
+^---^------------^----------^-----------------^-----^-----------------
+_f_: forward     _→_:          slurp forward   _R_: splice
+_b_: backward    _←_:          barf forward    _r_: raise
+_u_: backward ↑  _C-<right>_:  slurp backward  _↑_: raise backward
+_d_: forward ↓   _C-<left>_:   barf backward   _↓_: raise forward
+_p_: backward ↓
+_n_: forward ↑
+^Kill^           ^Misc^                       ^Wrap^
+^----^-----------^----^-----------------------^----^------------------
+_w_: copy        _j_: join                    _(_: wrap with ( )
+_k_: kill        _s_: split                   _{_: wrap with { }
+^^               _t_: transpose               _'_: wrap with ' '
+^^               _c_: convolute               _\"_: wrap with \" \"
+^^               _i_: indent defun"
+    ("q" nil)
+    ;; Wrapping
+    ("(" (lambda (_) (interactive "P") (sp-wrap-with-pair "(")))
+    ("{" (lambda (_) (interactive "P") (sp-wrap-with-pair "{")))
+    ("'" (lambda (_) (interactive "P") (sp-wrap-with-pair "'")))
+    ("\"" (lambda (_) (interactive "P") (sp-wrap-with-pair "\"")))
+    ;; Navigation
+    ("f" sp-forward-sexp )
+    ("b" sp-backward-sexp)
+    ("u" sp-backward-up-sexp)
+    ("d" sp-down-sexp)
+    ("p" sp-backward-down-sexp)
+    ("n" sp-up-sexp)
+    ;; Kill/copy
+    ("w" sp-copy-sexp)
+    ("k" sp-kill-sexp)
+    ;; Misc
+    ("t" sp-transpose-sexp)
+    ("j" sp-join-sexp)
+    ("s" sp-split-sexp)
+    ("c" sp-convolute-sexp)
+    ("i" sp-indent-defun)
+    ;; Depth changing
+    ("R" sp-splice-sexp)
+    ("r" sp-splice-sexp-killing-around)
+    ("<up>" sp-splice-sexp-killing-backward)
+    ("<down>" sp-splice-sexp-killing-forward)
+    ;; Barfing/slurping
+    ("<right>" sp-forward-slurp-sexp)
+    ("<left>" sp-forward-barf-sexp)
+    ("C-<left>" sp-backward-barf-sexp)
+    ("C-<right>" sp-backward-slurp-sexp))
 
-(add-hook 'python-mode-hook 'turn-on-smartparens-strict-mode)
-(add-hook 'c-mode-hook 'turn-on-smartparens-strict-mode)
-(add-hook 'emacs-lisp-mode-hook 'turn-on-smartparens-strict-mode)
-(add-hook 'lisp-mode-hook 'turn-on-smartparens-strict-mode)
-(setq sp-cancel-autoskip-on-backward-movement nil)
+  (smartparens-global-mode)
+  (show-smartparens-global-mode)
+
+  (dolist (hook '(inferior-emacs-lisp-mode-hook
+                  emacs-lisp-mode-hook))
+    (add-hook hook #'smartparens-strict-mode))
+  :config
+  (require 'smartparens-config)
+
+  (setq sp-autoskip-closing-pair 'always
+	;; Don't kill entire symbol on C-k
+	sp-hybrid-kill-entire-symbol nil)
+
+  ;; C++ pair management
+  (sp-with-modes '(c++-mode)
+    (sp-local-pair "{" nil :post-handlers '(("||\n[i]" "RET"))))
+  (sp-local-pair 'c++-mode "/*" "*/" :post-handlers '((" | " "SPC")
+  						      ("* ||\n[i]" "RET")))
+
+  (setq sp-cancel-autoskip-on-backward-movement nil))
+
+(use-package hydra-smartparens
+  :disabled t
+  :after smartparens
+  :config (hydra-smartparens-bind-keys))
 
 (provide 'init-smartparens)
