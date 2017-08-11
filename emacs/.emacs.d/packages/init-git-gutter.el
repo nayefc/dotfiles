b65462d9fa27db06cc5231da846b7263aaf1af19
Nayef Copty <nrcopty@gmail.com>
2017-08-11 12:00:56 -0400

    Defer git-gutter loading.

--- emacs/.emacs.d/packages/init-git-gutter.el
+++ emacs/.emacs.d/packages/init-git-gutter.el
@@ -1,7 +1,9 @@
 (use-package git-gutter+
   :ensure t
-  :diminish (git-gutter+-mode "gg")
-  :init
-  (add-hook 'python-mode-hook 'git-gutter+-mode))
+  :defer t
+  :diminish (git-gutter+-mode "gg"))
+
+(add-hook 'python-mode-hook 'git-gutter+-mode)
+(add-hook 'c-mode-common-hook 'git-gutter+-mode)
 
 (provide 'init-git-gutter)
