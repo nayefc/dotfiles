e6674e8d5dfcb3826f0a455b484041d15e5d622c
Nayef Copty <nrcopty@gmail.com>
2017-08-24 11:07:27 -0400

    More netotree fixes.

--- emacs/.emacs.d/packages/init-neotree.el
+++ emacs/.emacs.d/packages/init-neotree.el
@@ -2,7 +2,7 @@
   :ensure t
   :defer t
   :init
-  ;; (setq projectile-switch-project-action 'neotree-projectile-action)
+  (setq projectile-switch-project-action 'neotree-projectile-action)
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
   (setq neo-smart-open t)
   :config
