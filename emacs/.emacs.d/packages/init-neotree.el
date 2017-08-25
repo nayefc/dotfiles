d936ac5da8887daf3d5ec72f653ad855fd49e94b
Nayef Copty <nrcopty@gmail.com>
2017-08-25 13:09:08 -0400

    last one.

--- emacs/.emacs.d/packages/init-neotree.el
+++ emacs/.emacs.d/packages/init-neotree.el
@@ -2,6 +2,7 @@
   :ensure t
   :defer t
   :init
+  ;;(setq projectile-switch-project-action 'neotree-projectile-action)
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
   (setq neo-smart-open t)
   (setq neo-window-fixed-size nil))
