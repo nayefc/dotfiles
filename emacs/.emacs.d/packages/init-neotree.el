9556260dfbeb91e22bada894ae0d833822e98cf3
Nayef Copty <nrcopty@gmail.com>
2017-08-25 10:18:39 -0400

    More neotree fixes.

--- emacs/.emacs.d/packages/init-neotree.el
+++ emacs/.emacs.d/packages/init-neotree.el
@@ -2,10 +2,8 @@
   :ensure t
   :defer t
   :init
-  (setq projectile-switch-project-action 'neotree-projectile-action)
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
   (setq neo-smart-open t)
-  :config
-  (neo-global--set-window-width 30))
+  (setq neo-window-fixed-size nil))
 
 (provide 'init-neotree)
