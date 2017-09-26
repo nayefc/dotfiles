be2022c3b23605ae7f596aaf5f159fd8588e03f2
Nayef Copty <nrcopty@gmail.com>
2017-09-26 11:02:41 -0400

    Add Yasnippet.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -97,6 +97,8 @@
   :load-path "packages/")
 (use-package init-ag
   :load-path "packages/")
+(use-package init-yasnippet
+  :load-path "packages/")
 
 ;; python packages
 (use-package init-jedi
@@ -135,9 +137,9 @@
  '(exec-path-from-shell-check-startup-files nil)
  '(package-selected-packages
    (quote
-    (levenshtein pytest realgud package-build shut-up epl git commander f dash s virtualenvwrapper elisp--witness--lisp string-inflection shx
-		 (progn t elisp--witness--lisp)
-		 smartparens ivy-rtags rtags all-the-icons-ivy protobuf-mode all-the-icons all-the-icons-dired neotree ivy-hydra indent-tools irony projectile-speedbar project-explorer ag dumb-jump dashboard flycheck-irony counsel-projectile company-irony-c-headers company-irony google-c-style ivy buffer-move pylint highlight-indentation company-jedi jedi expand-region company projectile exec-path-from-shell fill-column-indicator git-gutter+ flycheck highlight-symbol multiple-cursors ace-window avy magit yaml-mode with-editor use-package sr-speedbar solarized-theme pallet magit-popup))))
+    (yasnippet-snippets yasnippet levenshtein pytest realgud package-build shut-up epl git commander f dash s virtualenvwrapper elisp--witness--lisp string-inflection shx
+			(progn t elisp--witness--lisp)
+			smartparens ivy-rtags rtags all-the-icons-ivy protobuf-mode all-the-icons all-the-icons-dired neotree ivy-hydra indent-tools irony projectile-speedbar project-explorer ag dumb-jump dashboard flycheck-irony counsel-projectile company-irony-c-headers company-irony google-c-style ivy buffer-move pylint highlight-indentation company-jedi jedi expand-region company projectile exec-path-from-shell fill-column-indicator git-gutter+ flycheck highlight-symbol multiple-cursors ace-window avy magit yaml-mode with-editor use-package sr-speedbar solarized-theme pallet magit-popup))))
 
 ;; Font height 135 or 130
 (custom-set-faces
