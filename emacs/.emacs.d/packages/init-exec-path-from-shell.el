2cc39ff38b3979e13af766e703d5e5c75b1c935b
Nayef Copty <nrcopty@gmail.com>
2019-12-27 09:44:42 -0500

    exec-path-from-shell fix.

--- emacs/.emacs.d/packages/init-exec-path-from-shell.el
+++ emacs/.emacs.d/packages/init-exec-path-from-shell.el
@@ -4,6 +4,7 @@
   (custom-set-variables
    '(exec-path-from-shell-check-startup-files nil))
   :config
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-envs '("LANG")))
 
 (provide 'init-exec-path-from-shell)
